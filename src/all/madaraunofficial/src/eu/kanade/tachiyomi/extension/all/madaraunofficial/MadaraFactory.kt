50aacd17a7713a1f7283b48b41ecc8f32abd0203
Aria Moradi <aria.moradi007@gmail.com>
2021-01-15 14:11:37 +0330

    gaa

---
+++ src/all/madaraunofficial/src/eu/kanade/tachiyomi/extension/all/madaraunofficial/MadaraFactory.kt
@@ -0,0 +1,24 @@
+package eu.kanade.tachiyomi.extension.all.madaraunofficial
+
+import eu.kanade.tachiyomi.source.Source
+import eu.kanade.tachiyomi.source.SourceFactory
+
+class MadaraFactory : SourceFactory {
+    override fun createSources(): List<Source> = listOf(
+        MangaGecesi(),
+        MangaWOW(),
+        AhStudios(),
+        KnightNoScanlation(),
+    )
+}
+
+class AhStudios : Madara("AhStudios", "https://ahstudios.net", "es")
+
+
+class MangaGecesi : Madara("Manga Gecesi", "https://mangagecesi.com", "tr") {
+    override val chapterUrlSelector = "li.wp-manga-chapter div.chapter-thumbnail + a"
+}
+
+class MangaWOW : Madara("MangaWOW", "https://mangawow.com", "tr")
+
+class KnightNoScanlation : Madara("Knight no Scanlation", "https://knightnoscanlation.com", "es")
